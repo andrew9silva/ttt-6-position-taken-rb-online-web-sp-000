a7e4e56f809f806fea7397d354910cf06504c937
Andrew Silva <andrewsilva9@gmail.com>
2019-04-01 23:49:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
+    false
+  elsif board == ["", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
+    false
+  elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] && index == 0 || 1
+    false
+  else board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && index == 0 || 8 
+    true
+  end
+end
