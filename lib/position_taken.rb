c17348d5772fa7e7a18ed084a248c2d74626d446
Andrew Silva <andrewsilva9@gmail.com>
2019-04-01 23:54:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,4 +2,6 @@
 def position_taken?(board, integer)
    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] && integer == 0
   false
+  board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && integer == 0 || 8
+  true 
 end
