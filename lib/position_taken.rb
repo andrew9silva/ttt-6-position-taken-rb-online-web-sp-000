9dcf486f82139e4f1cf627d0e75b857bccc68932
Andrew Silva <andrewsilva9@gmail.com>
2019-04-01 23:59:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, integer)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] && integer == 0
+  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] && integer == 0
   false
 end
