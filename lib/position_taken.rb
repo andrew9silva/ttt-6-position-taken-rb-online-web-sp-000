98d7ee881f4523c48039326458df5c4d417b3637
Andrew Silva <andrewsilva9@gmail.com>
2019-04-02 00:40:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,10 @@
 require "pry"
 def position_taken?(board, integer)
   binding.pry
-  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] && integer == 0
+  if board[integer] == " " || board[integer] == "" || board[integer] == nil 
   false
-elsif board = [nil, " ", " ", " ", " ", " ", " ", " ", " "] && integer == 0 || 1
-  false
-else board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && integer == 0 || 8
+
+elsif board[integer] == "X" || board[integer] == "O"
   true
 end
 end
